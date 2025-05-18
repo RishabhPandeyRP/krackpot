63659c75ccd989665b769cc8af6de8d6fb4686df
RishabhPandeyRP <rishabh17704@gmail.com>
2025-05-18 10:05:28 +0530

    commit

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -22,45 +22,37 @@ const DropDown = ({ data }: {
         <div className="lg:w-[80%] sm:w-[85%] mx-auto border-t-1 border-[#01193D] bg-white" onClick={() => { setIsopen(!isOpen) }}>
             <div className="w-[100%] h-fit flex items-center gap-[3%] relative">
                 <div className=" w-fit leading-28 flex gap-5">
-                    <div className="font-auster text-shadow text-stroke font-[600] lg:text-[55px] md:text-[45px] sm:text-[35px] text-[#DE9400]">
+                    <div className="font-auster text-shadow text-stroke font-[600] lg:text-[55px] md:text-[35px] sm:text-[25px] text-[#DE9400]">
                         {data.heading}
                     </div>
 
 
-                    <div className="font-auster font-[500] lg:text-[55px] md:text-[45px] sm:text-[35px] text-[#01193D] w-fit leading-28">
+                    <div className="font-auster font-[500] lg:text-[55px] md:text-[35px] sm:text-[25px] text-[#01193D] w-fit leading-28">
                         :
                     </div>
                 </div>
 
 
 
-                <div className="flex gap-2">
+                <div className="flex flex-wrap gap-2">
                     {
-                        data.sub_headings.map((item , index) => (
-                            <div className="font-auster font-[600] lg:text-[22px] sm:text-[18px] text-[#01193D] w-fit before:content-['•'] before:mr-2 whitespace-nowrap " key={index}>
+                        data.sub_headings.map((item, index) => (
+                            item.heading != "" && <div className="font-auster font-[600] lg:text-[22px] sm:text-[18px] text-[#01193D] w-fit before:content-['•'] before:mr-2 whitespace-nowrap " key={index}>
                                 {item.heading}
                             </div>
                         ))
                     }
                 </div>
 
-                <button className="text-black absolute right-10" onClick={btnHandler}>
-                    {
-                        isOpen ? <Image
-                            src={"/up.svg"}
-                            alt=""
-                            width={50}
-                            height={50}
+                <div className="absolute right-[3%] w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10">
+                    <Image
+                        src={isOpen ? "/up.svg" : "/down.svg"}
+                        alt="Toggle Icon"
+                        fill
+                        className="object-contain"
+                    />
+                </div>
 
-                        ></Image> : <Image
-                            src={"/down.svg"}
-                            alt=""
-                            width={50}
-                            height={50}
-
-                        ></Image>
-                    }
-                </button>
 
 
 
@@ -72,13 +64,13 @@ const DropDown = ({ data }: {
             >
                 {data.sub_headings.map((item, idx) => (
                     <div key={idx} className="w-full py-2 flex gap-5">
-                        <div className="font-auster font-[600] text-[25px] text-[#01193D] w-fit whitespace-nowrap border-0 border-red-500 ">
+                        {item.heading != "" && <div className="font-auster font-[600] lg:text-[25px] md:text-[20px] sm:text-[15px] text-[#01193D] w-fit whitespace-nowrap border-0 border-red-500 ">
                             {item.heading} :-
-                        </div>
+                        </div>}
 
                         <div className="flex flex-wrap gap-4 border-0 border-red-500">
                             {item.sub_headings.map((content, i) => (
-                                <span key={i} className="font-auster font-[500] text-[25px] text-[#01193D] w-fit whitespace-nowrap before:content-['•'] before:mr-2">
+                                <span key={i} className="font-auster font-[500] lg:text-[25px] md:text-[20px] sm:text-[15px] text-[#01193D] w-fit whitespace-nowrap before:content-['•'] before:mr-2">
                                     {content}
                                 </span>
                             ))}
